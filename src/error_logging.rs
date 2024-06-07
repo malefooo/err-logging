ce1490213a4f265dc9d16322305b559f798c75e5
malefooo <784479660@qq.com>
2024-06-07 09:46:14 +0800

    remove

--- src/error_logging.rs
+++ src/error_logging.rs
@@ -2,7 +2,7 @@ pub trait ErrorLogging {
     type ResultType;
 
     /// uuid, file, line
-    fn elog(self, ctx: (&str, &str, u32)) -> Self::ResultType;
+    fn elog(self, ctx: (Option<&str>, &str, u32)) -> Self::ResultType;
 }
 
 impl<T, E> ErrorLogging for Result<T, E>
@@ -11,10 +11,17 @@ where
 {
     type ResultType = Result<T, E>;
 
-    fn elog(self, ctx: (&str, &str, u32)) -> Self::ResultType {
+    fn elog(self, ctx: (Option<&str>, &str, u32)) -> Self::ResultType {
         self.map_err(|err| {
+
             let (uid, file, line) = ctx;
-            log::error!("[ <{uid}> | <{file}> | <{line}> ] - [{err:?}]");
+
+            if let Some(uid) = uid {
+                log::error!("[ <{uid}> | <{file}> | <{line}> ] - [{err:?}]");
+            } else {
+                log::error!("[ <{file}> | <{line}> ] - [{err:?}]");
+            }
+
             err
         })
     }
@@ -22,39 +29,19 @@ where
 
 #[macro_export]
 macro_rules! ctx {
+    ($uid:expr) => {
+        (
+            Some($uid),     // uid
+            file!(),        // cur file
+            line!(),        // cur line
+        )
+    };
     () => {
         (
-            &sync_get(), // uid
-            file!(),     // cur file
-            line!(),     // cur line
+            None,    // no uid
+            file!(), // cur file
+            line!(), // cur line
         )
     };
 }
 
-#[macro_export]
-macro_rules! actx {
-    () => {
-        (
-            &async_get(), // uid
-            file!(),      // cur file
-            line!(),      // cur line
-        )
-    };
-}
-
-#[cfg(test)]
-mod test {
-    use crate::error_logging::ErrorLogging;
-    use crate::sync_uid::sync_get;
-    use std::env;
-
-    #[test]
-    fn test_ep() {
-        env::set_var("RUST_LOG", "debug");
-
-        env_logger::init();
-
-        let r: Result<u64, &str> = Err("test ep print");
-        r.elog(ctx!()).unwrap_or(0);
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,1 @@
-pub mod async_uid;
 pub mod error_logging;
-pub mod sync_uid;
